78165af8d680bd1fbeb780cf9d5b702739da66bf
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-01-20 10:51:51 +0100

    Fixed positioning of image and hiding loader on loaded video

--- receiver.js
+++ receiver.js
@@ -25,6 +25,7 @@ customMessageBus.onMessage = function(event) {
         }
         if(initial){
           $("#player").toggleClass("hide");
+          $(".uil-ring-css").toggleCass("hide");
           $("#zoff-logo").toggleClass("center");
           $("#zoff-logo").toggleClass("lower_left");
           initial = false;
